54d1d7550709c2d08e2e3a0f6c460fad92f0e5eb
mateuszkrukowski <57400658+mateuszkrukowski@users.noreply.github.com>
2023-03-14 12:15:19 +0100

    Update my_timer.sh

--- my_timer.sh
+++ my_timer.sh
@@ -1,6 +1,6 @@
 echo -n "Enter time in minutes: "
 read time_interval
 calculated=$(($time_interval * 60))
-echo "Timer is set to  $time_interval minutes"
+echo "Timer is set to $time_interval minutes"
 sleep $calculated
 shutdown
