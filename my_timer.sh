33c0c2f73c59b15612cddff5677fe8519dedce20
random <random@host13.home>
2023-03-15 12:00:59 +0100

    Improved shuting down for time equal to 1 minute

--- my_timer.sh
+++ my_timer.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 echo -n "Enter time in minutes: "
 read time_interval
 calculated=$(($time_interval * 60))
 echo "Timer is set to $time_interval minutes"
-sleep $calculated
-shutdown
+if [ $time_interval -eq 1 ]
+then
+    shutdown
+else
+    sleep $calculated
+    shutdown
+fi
